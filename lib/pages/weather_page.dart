44601ddaa93db39cd4b8b90b875830b246e3bca3
unknown <mrindistress@gmail.com>
2026-02-20 21:17:07 +0600

    adding the project folder to repo \n it shows the weather , no themes

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weatherapp/service/weather_service.dart';
+import 'package:weatherapp/models/weather_model.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService(
+    apiKey: "7dfbe705c618e04790ab0272595ec62a",
+  );
+
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.GetCityName();
+
+    try {
+      final weather = await _weatherService.GetWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String lottieAnimation_location = "assets/Weather-partly shower.json";
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //!show city Name
+            Text(
+              _weather?.cityName ?? "loading city...",
+              style: GoogleFonts.jetBrainsMono(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+
+            Lottie.asset(lottieAnimation_location),
+
+            //!show temperature
+            Text(
+              ('${_weather?.tempreature.toString()}°C'),
+
+              style: GoogleFonts.jetBrainsMono(
+                fontWeight: FontWeight.bold,
+                fontSize: 45,
+              ),
+            ),
+
+            Text(
+              _weather?.mainCondition ?? "loading",
+              style: GoogleFonts.jetBrainsMono(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/service/weather_service.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weatherapp/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+  final String apiKey;
+
+  WeatherService({required this.apiKey});
+
+  Future<Weather> GetWeather(String cityName) async {
+    final response = await http.get(
+      Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'),
+    );
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("failed to load data");
+    }
+  }
+
+  Future<String> GetCityName() async {
+
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception("Location services are disabled.");
+    }
+
+    //! get permission from user about location
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    //! fetch the x,y position of user after getting permission
+      Position position = await Geolocator.getCurrentPosition();
+
+    //!get cityName from x,y coords
+      List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+
+      String ? cityName = placemarks[0].locality;
+
+
+    return cityName ?? "";
+  }
+}
